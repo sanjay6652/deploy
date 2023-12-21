7c83cf1782718a55c26de53cfd3434b0ad6ebe66
sanjay.b <sanjay.b@tadigital.com>
2023-12-21 11:44:36 +0530

    trigger removed

--- src/main/java/com/nexus/customerimport/Controller/CSVController.java
+++ src/main/java/com/nexus/customerimport/Controller/CSVController.java
@@ -1,22 +1,22 @@
-package com.nexus.customerimport.Controller;
-
-import com.nexus.customerimport.Service.CSVService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/csv")
-public class CSVController {
-
-    private final CSVService csvService;
-
-    public CSVController(CSVService csvService) {
-        this.csvService = csvService;
-    }
-
-    @GetMapping("/process")
-    public void startHotFolder() {
-        csvService.startHotFolder("C:\\Users\\SanjayB(TADigital)\\Desktop\\hotfolder");
-    }
-}
+//package com.nexus.customerimport.Controller;
+//
+//import com.nexus.customerimport.Service.CSVService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/csv")
+//public class CSVController {
+//
+//    private final CSVService csvService;
+//
+//    public CSVController(CSVService csvService) {
+//        this.csvService = csvService;
+//    }
+//
+//    @GetMapping("/process")
+//    public void startHotFolder() {
+//        csvService.startHotFolder("C:\\Users\\SanjayB(TADigital)\\Desktop\\hotfolder");
+//    }
+//}
--- src/main/java/com/nexus/customerimport/CustomerImportApplication.java
+++ src/main/java/com/nexus/customerimport/CustomerImportApplication.java
@@ -1,5 +1,6 @@
 package com.nexus.customerimport;
 
+import com.nexus.customerimport.Service.CSVService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,11 @@ import com.commercetools.api.models.project.*;
 public class CustomerImportApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerImportApplication.class, args);
+		someMethod();
+	}
+	public static void someMethod() {
+		CSVService csvService = new CSVService();
+		csvService.startHotFolder("C:\\Users\\SanjayB(TADigital)\\Desktop\\hotfolder");
 	}
 
 
